72e5b3bcee342f209a08e8b2a580ff2442003038
ehalliez <hlzemile@gmail.com>
2025-01-07 15:23:57 +0100

    feat: enhance scatter plot function with detailed docstring and error handling

--- scatter_plot.py
+++ scatter_plot.py
@@ -6,20 +6,43 @@ import sys
 from utils import load_csv
 
 def scatter_plot(csv_file: pd.DataFrame, feature1: str, feature2: str):
-    data = csv_file.dropna()
-    HogwartsHouse = {
-        'Gryffindor': (data.loc[data['Hogwarts House'] == 'Gryffindor'], 'red'),
-        'Slytherin': (data.loc[data['Hogwarts House'] == 'Slytherin'], 'green'),
-        'Ravenclaw': (data.loc[data['Hogwarts House'] == 'Ravenclaw'], 'blue'),
-        'Hufflepuff': (data.loc[data['Hogwarts House'] == 'Hufflepuff'], 'yellow')
-    }
-    for house, (dataFrame, color) in HogwartsHouse.items():
-        plt.scatter(dataFrame[feature1], dataFrame[feature2], label=house, color=color)
-    plt.legend()
-    plt.title(f'Scatter plot : {feature1} vs {feature2}')
-    plt.xlabel(feature1)
-    plt.ylabel(feature2)
-    plt.show()
+    """
+    Generates a scatter plot for two features from a given DataFrame, colored by Hogwarts House.
+
+    Parameters:
+    csv_file (pd.DataFrame): The input DataFrame containing the data.
+    feature1 (str): The name of the first feature to plot on the x-axis.
+    feature2 (str): The name of the second feature to plot on the y-axis.
+
+    Returns:
+    None
+
+    Raises:
+    KeyError: If the specified features are not found in the DataFrame.
+
+    The scatter plot will display points colored by Hogwarts House:
+    - Gryffindor: red
+    - Slytherin: green
+    - Ravenclaw: blue
+    - Hufflepuff: yellow
+    """
+    try:
+        data = csv_file.dropna()
+        HogwartsHouse = {
+            'Gryffindor': (data.loc[data['Hogwarts House'] == 'Gryffindor'], 'red'),
+            'Slytherin': (data.loc[data['Hogwarts House'] == 'Slytherin'], 'green'),
+            'Ravenclaw': (data.loc[data['Hogwarts House'] == 'Ravenclaw'], 'blue'),
+            'Hufflepuff': (data.loc[data['Hogwarts House'] == 'Hufflepuff'], 'yellow')
+        }
+        for house, (dataFrame, color) in HogwartsHouse.items():
+            plt.scatter(dataFrame[feature1], dataFrame[feature2], label=house, color=color)
+        plt.legend()
+        plt.title(f'Scatter plot : {feature1} vs {feature2}')
+        plt.xlabel(feature1)
+        plt.ylabel(feature2)
+        plt.show()
+    except KeyError as e:
+        print(f"Please put a valid feature: {e}")
 
 def main():
     try:
