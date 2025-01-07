c9efb03c820fd5058ff83229743bdd89b4c90183
ehalliez <hlzemile@gmail.com>
2025-01-07 14:45:09 +0100

    feat: refactor scatter plot functionality with argument parsing and house color coding

--- scatter_plot.py
+++ scatter_plot.py
@@ -1,26 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
+import argparse as args
 import sys
+from utils import load_csv
+
+def scatter_plot(csv_file: pd.DataFrame, feature1: str, feature2: str):
+    data = csv_file.dropna()
+    HogwartsHouse = { 
+        'Gryffindor': {
+            'dataFrame': data.loc[data['Hogwarts House'] == 'Gryffindor'],
+            'color': 'red'
+        },
+        'Slytherin': {
+            'dataFrame': data.loc[data['Hogwarts House'] == 'Slytherin'],
+            'color': 'green'
+        },
+        'Ravenclaw': {
+            'dataFrame': data.loc[data['Hogwarts House'] == 'Ravenclaw'],
+            'color': 'blue'
+        },
+        'Hufflepuff': {
+            'dataFrame': data.loc[data['Hogwarts House'] == 'Hufflepuff'],
+            'color': 'yellow'
+        }
+    }
+    for house, items in HogwartsHouse.items():
+        x = items['dataFrame'][feature1]
+        y = items['dataFrame'][feature2]
+        plt.scatter(x, y, label=house, color=items['color'])
+    plt.legend()
+    plt.title(f'Scatter plot : {feature1} vs {feature2}')
+    plt.xlabel(feature1)
+    plt.ylabel(feature2)
+    plt.show()
 
 def main():
     try:
-        if len(sys.argv) != 4:
-            print("Usage: python scatter_plot.py <dataset.csv> <feature1> <feature2>")
-            sys.exit(1)
-        dataset = pd.read_csv(sys.argv[1])
-        feature1 = sys.argv[2]
-        feature2 = sys.argv[3]
-        if feature1 not in dataset.columns:
-            print(f"{feature1} not in dataset")
-            sys.exit(1)
-        if feature2 not in dataset.columns:
-            print(f"{feature2} not in dataset")
-            sys.exit(1)
-        plt.scatter(dataset[feature1], dataset[feature2])
-        plt.xlabel(feature1)
-        plt.ylabel(feature2)
-        plt.title(f"{feature1} vs {feature2}")
-        plt.show()
+        parser = args.ArgumentParser(description="usage: python3 scatter_plot.py --feature1 \"Astronomy\" --feature2 \"Arithmancy\"")
+        parser.add_argument('--file', type=str, help="location of the dataset", required = True)
+        parser.add_argument('--feature1', type=str, help="name of the feature", required = True)
+        parser.add_argument('--feature2', type=str, help="name of the feature", required = True)
+        arg = parser.parse_args()
+        csv_file = load_csv.load(arg.file)
+        scatter_plot(csv_file, arg.feature1, arg.feature2)
     except FileNotFoundError as e:
         print(e)
 
